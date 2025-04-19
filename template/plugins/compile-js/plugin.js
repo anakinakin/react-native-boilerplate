fe98819caeb88530bacb322c8c68369188f4540a
anakinakin <128812154+anakinakin@users.noreply.github.com>
2025-04-19 02:19:45 +0200

    Update plugin.js

--- template/plugins/compile-js/plugin.js
+++ template/plugins/compile-js/plugin.js
@@ -80,13 +80,9 @@ module.exports = {
 
           console.log('♻️  Replacing source...');
           execSync('rm -rf src', { stdio: 'pipe' });
-          console.log('1');
           execSync('cp -R js/src ./src', { stdio: 'pipe' });
-          console.log('2');
-          execSync('rm -rf __mocks__', { stdio: 'pipe' });
-          console.log('3');
-          execSync('cp -R js/__mocks__ ./__mocks__', { stdio: 'pipe' });
-          console.log('4');
+          execSync('rm -rf tests/__mocks__', { stdio: 'pipe' });
+          execSync('cp -R js/tests/__mocks__ ./tests/__mocks__', { stdio: 'pipe' });
           execSync('rm -rf js', { stdio: 'pipe' });
         } catch {
           console.error(
